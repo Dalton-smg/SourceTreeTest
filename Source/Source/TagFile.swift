3e0f73c41a4e8016e09e68c9ec6c96c510b2629e
Dalton <daltonnguyen@nvg.vn>
2023-05-31 17:27:46 +0700

    Create Tag File

---
+++ Source/Source/TagFile.swift
@@ -0,0 +1,17 @@
+//
+//  TagFile.swift
+//  Source
+//
+//  Created by Dalton Nguyen on 31/05/2023.
+//
+
+import Foundation
+
+
+class TagFile {
+    var tag: String
+    
+    init(tag: String) {
+        self.tag = tag
+    }
+}
